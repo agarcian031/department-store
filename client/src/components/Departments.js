963ba492405ac318a6a216f4ad10e860b8ba3250
Amanda Garcia <algarcian@outlook.com>
2019-06-13 17:26:19 -0600

    FIXME: All cards toggle on edit click

--- client/src/components/Departments.js
+++ client/src/components/Departments.js
@@ -1,84 +1,95 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'; 
-import axios from 'axios'; 
-import {Card, Header, Divider, Button, Icon} from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Card, Header, Divider, Button, Icon, DropdownHeader } from "semantic-ui-react";
+import DepartmentForm from "./DepartmentEditForm";
 
 const Departments = (props) => {
-  const [departments, setDepartments] = useState([]); 
-  // const [editing, setEditing] = useState([false]); 
+  const [
+    departments,
+    setDepartments
+  ] = useState([]);
+  const [
+    editing,
+    setEditing
+  ] = useState([
+    false
+  ]);
 
-
-  useEffect( () => { 
-    axios.get("/api/departments")
-    .then( res => {
+  useEffect(() => {
+    axios.get("/api/departments").then((res) => {
       setDepartments(res.data);
     });
-  }, []); 
+  }, []);
 
-
-  // FIXME - will delete the item and the product only if refreshed 
+  // FIXME - will delete the item and the product only if refreshed
   // FIXED = passing in id in `` caused the id to be read as a string
   const deleteDepartment = (id) => {
     // const id = props.match.params.department_id
-    axios.delete(`/api/departments/${id}`)
-    .then(res=>{
-      // Error -> created a new departments array 
-    //  setDepartments({ departments: departments.filter( dep => dep.id !== id)})
-     setDepartments(departments.filter( dep => dep.id !== id))
+    axios.delete(`/api/departments/${id}`).then((res) => {
+      // Error -> created a new departments array
+      //  setDepartments({ departments: departments.filter( dep => dep.id !== id)})
+      setDepartments(departments.filter((dep) => dep.id !== id));
     });
-  }
+  };
 
-  // const toggleEdit = () => {
-  //   setEditing(!editing)
-  //   console.log({editing}); 
-  // }
+  const toggleEdit = () => {
+    setEditing(!editing);
+    console.log({ editing });
+  };
 
-
-
-
-
- const renderDepartments = () => {
+  const renderDepartments = () => {
     if (departments.length <= 0)
-      return <Header as="h3" textAlign="center">No Departments Listed</Header>
-    return departments.map( department => (
+      return (
+        <Header as="h3" textAlign="center">
+          No Departments Listed
+        </Header>
+      );
+    return departments.map((department) => (
       <Card color="blue" key={department.id}>
-        <Card.Content>
-          <Card.Header textAlign="center">{department.name}</Card.Header>
-        </Card.Content>
+        {editing ? (
+          <Card.Content>
+            <DepartmentForm {...props} edit={props.edit} toggleEdit={toggleEdit} />
+          </Card.Content>
+        ) : (
+          <Card.Content>
+            <Card.Header textAlign="center">{department.name}</Card.Header>
+          </Card.Content>
+        )}
+
         <Card.Content extra>
           <Button.Group size="tiny" fluid>
-          <Button as={Link} to={`/departments/${department.id}`} color='blue' size="tiny">
-            View
-          </Button>
-          <Button icon as={Link} to={`/departments/edit`}>
-          {/* <Button icon onClick={() => toggleEdit()}> */}
-            <Icon name="pencil"/> Edit 
-          </Button>
-          <Button icon color="red" onClick={() => deleteDepartment(department.id)}>
-            <Icon name="trash"/> Delete
-          </Button>
+            <Button as={Link} to={`/departments/${department.id}`} color="blue" size="tiny">
+              View
+            </Button>
+            {/* <Button icon as={Link} to={`/departments/edit`}> */}
+            <Button icon onClick={() => toggleEdit()}>
+              <Icon name="pencil" /> Edit
+            </Button>
+            <Button icon color="red" onClick={() => deleteDepartment(department.id)}>
+              <Icon name="trash" /> Delete
+            </Button>
           </Button.Group>
         </Card.Content>
       </Card>
-    ))
-  }
-
+    ));
+  };
 
   return (
     <div>
-      <Header as="h1" textAlign="center">Departments</Header>
-      <Divider/>
-      <br/>
-      <Button as={Link} to="/departments/new" icon size="tiny" color="blue"> 
-        <Icon name="plus"/>Add Departments
+      <Header as="h1" textAlign="center">
+        Departments
+      </Header>
+      <Divider />
+      <br />
+      <Button as={Link} to="/departments/new" icon size="tiny" color="blue">
+        <Icon name="plus" />Add Departments
       </Button>
-      <br/>
-      <br/>
-      <Card.Group>
-        {renderDepartments()}
-      </Card.Group>
+      <br />
+      <br />
+      <Card.Group>{renderDepartments()}</Card.Group>
     </div>
-  )
-}
+  );
+};
 
-export default Departments; 
+export default Departments;
